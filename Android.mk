202c85ba7389c2b12573031b5c462298cba91de4
SphericalKat <amolele@gmail.com>
2026-09-21 19:27:58 +0530

    volkswagen: Initial vendor import
    Extracted with device/oneplus/volkswagen/extract-files.py from
    CPH2793_16.0.5.1200(EX01), build BP2A.250605.015.
    
    radio/*, the ArcSoft turbo libraries, and the face restore model are
    stored with Git LFS.
    
    Signed-off-by: SphericalKat <amolele@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,38 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),volkswagen)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,a21217a15832a645825f972884c62250c5ca8406)
+$(call add-radio-file-sha1-checked,radio/aop.img,2935895e77f5281a78670bccbeed16e6e17b8396)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,313be3c9cbfe005d05903e9c2d0aec344d52c675)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,d3f304cfe0304b6f05c18375402f85166070a35d)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,dc3f841d8a00409d935728e68c8ccbd8e48afb33)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,bc6a33e7c67b2dddc009e26a1eb9bd82a5499696)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,923fc4bc06c06fee468b593cb2f5364afbab20b0)
+$(call add-radio-file-sha1-checked,radio/dsp.img,19c71686dbfac45ede5073d52d199c3c81896c7c)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,ee66ca1e1bf4e4bbb52e01c454377ed62272fe65)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,6885d316f148ec74d4991408b9e16a510adcc529)
+$(call add-radio-file-sha1-checked,radio/hyp.img,e1dbc95ffa0e980bf78bdb917f61db70da176255)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,47c4c87d5e012f9f50af280831993403beb74db6)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,15fa30195cbf00440a69bff33213d24887c7be7b)
+$(call add-radio-file-sha1-checked,radio/modem.img,764bc2df9349f0c2b3b792dcc1375bdf36dff3f1)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,00d7b5c8f2ca7f8dc192ca449447cdad9df2cdf7)
+$(call add-radio-file-sha1-checked,radio/pvmfw.img,25b0645fe33fb7723e3325888eae8195bbe98ed6)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,6c592b53d9d9417afa3808d16df9ba13f80b7e3e)
+$(call add-radio-file-sha1-checked,radio/shrm.img,9cdd492b1a5dd24a534d883183dd577a4a1aab48)
+$(call add-radio-file-sha1-checked,radio/soccp_dcd.img,2f60a49af1a7b44c22da7a51ec2941a9fb192048)
+$(call add-radio-file-sha1-checked,radio/soccp_debug.img,84c3aae9e349be115ea156c06508d1bf6a016e4c)
+$(call add-radio-file-sha1-checked,radio/splash.img,30d46f9fd800be7c401a3a25bd9c2817c686e78e)
+$(call add-radio-file-sha1-checked,radio/spuservice.img,3e8e7aeda748eec6591cd3bbc5a60559ce9832b7)
+$(call add-radio-file-sha1-checked,radio/tz.img,f03552270868f29dd2e4c1cd883e866dfc479383)
+$(call add-radio-file-sha1-checked,radio/uefi.img,044592a4fec49a44733579b17d0efa4ab6257276)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,d771ae2f4cf2b0f2a1ca57386c623a435a5565cf)
+$(call add-radio-file-sha1-checked,radio/xbl.img,257478eba741d1887464f2249a6916bcefb5cf10)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,9eef63700b6459b35016b890135e6c01847e9bc2)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,719dbf598d4d774e3a6e1f2731c422c76805c4e0)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,33 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    engineering_cdt \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    oplus_sec \
+    pvmfw \
+    qupfw \
+    shrm \
+    soccp_dcd \
+    soccp_debug \
+    splash \
+    spuservice \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
